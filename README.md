8b3b4d0e3d2907713f61924dd6a4b9d054369f22
liviuirimia <liviu.florin.irimia@gmail.com>
2018-02-07 16:05:17 +0200

    7.02.2018

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # scripts
+
+# lazy.py
+alias webscan="grep -r \"POST /wp-login\" /var/log/httpd/access_log | awk '{ print \$1 }' | sort | uniq -c | awk '{ if(\$1 > 5) print \$2 }'"
+
+webscan | xargs -I{} /path/to/script {}
---
+++ lazy.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+ips = ([sys.argv[x+1] for x in xrange(len(sys.argv)-1)])
+nb = []
+
+FILE_PATH = "iptables.bak"
+SEARCH_STRING = "#add ip here\n"
+
+if __name__ == "__main__":
+	with open(FILE_PATH, 'r') as f:
+		for line in f:
+			if line == SEARCH_STRING:
+				line = line + "\n#" + time.ctime()
+				for ip in ips:
+					s = "-A INPUT -s " + ip + " -j DROP"
+					line = line + "\n" + s
+				line = line + "\n"
+			nb.append(line)
+
+	with open(FILE_PATH, 'w') as f:
+		f.seek(0)
+		f.truncate()
+
+		for line in nb:
+			f.write(line)
